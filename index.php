8f80e080be3eee5ea77d56532bc5407f99c057ae
agnestshibangu <74143439+agnestshibangu@users.noreply.github.com>
2021-05-02 03:02:52 +0200

    front part ok

--- index.php
+++ index.php
@@ -12,91 +12,82 @@
 </head>
 <body>
 
-<?php require_once 'process.php'; ?>
+<div class="container">
 
-<?php 
-    $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
-    $result = $mysqli->query("SELECT * FROM data ORDER by name ASC") or die($mysqli->error);
- 
-    
-    ?>
+        <?php require_once 'process.php'; ?>
 
-
-
-
+        <?php 
+            $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
+            $result = $mysqli->query("SELECT * FROM data ORDER by name ASC") or die($mysqli->error);
+        
+            
+        ?>
 
 
-<?php
+        <?php
 
-    function pre_r( $array ) {
-        echo '<pre>';
-        print_r($array);
-        echo '<pre>';
-    }
-?>
+            function pre_r( $array ) {
+                echo '<pre>';
+                print_r($array);
+                echo '<pre>';
+            }
+        ?>
 
+                        <div class="container-table">
 
+                            <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Location</th>
+                                            <th class="th-action">Action</th> 
+                                        </tr>
+                                    </thead>
 
+                            <?php
+                                while($row = $result->fetch_assoc()): ?>
+                                    <tr>
+                                        <td><?php echo $row['name']; ?></td>
+                                        <td><?php echo $row['location']; ?></td>
+                                        <td class="td-btn">
+                                            <a href="index.php?edit=<?php echo $row['id']; ?>"
+                                            class="btn btn-info btn-action">Edit</a>
+                                            <a href="index.php?delete=<?php echo $row['id']; ?>"
+                                            class="btn btn-delete btn-action">delete</a>
+                                        </td>
 
+                            <?php endwhile; ?>
 
-                <div class="container-table">
+                                    </tr>
+                                    
+                            </table>
 
-                    <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Location</th>
-                                    <th>Action</th> 
-                                </tr>
-                            </thead>
-
-                    <?php
-                        while($row = $result->fetch_assoc()): ?>
-                            <tr>
-                                <td><?php echo $row['name']; ?></td>
-                                <td><?php echo $row['location']; ?></td>
-                                <td>
-                                    <a href="index.php?edit=<?php echo $row['id']; ?>"
-                                    class="btn btn-info">Edit</a>
-                                    <a href="index.php?delete=<?php echo $row['id']; ?>"
-                                    class="btn btn-delete">delete</a>
-                                </td>
-
-                    <?php endwhile; ?>
-
-                            </tr>
-
-
-                    
+                        </div>
 
 
-                    </table>
+                    <div class="container-form">
 
-                </div>
+                        <form class="form" action="process.php" method="POST">
+                            
+                        <input type="hidden" name="id" value="<?php echo $id; ?> ">
+                        
+                        <input class="form-item input" type="text" name="name" 
+                            value="<?php echo $name; ?>" placeholder="Enter your name">
+                            <input class="form-item input" type="text" name="location"
+                            value="<?php echo $location; ?>"  placeholder="Enter your location">
+                            <?php 
+                                if ($update == true):
+                            ?>
+                                <button class="form-item btn btn-save btn-width" type="submit" name="update">Update</button>
+                        
+                            <?php else :?>
+                                <button class="form-item btn btn-save btn-width" type="submit" name="save">Save</button>
+                            <?php endif; ?>
+                        
+                        </form>
+                    </div>
 
-
-            <div class="container-form">
-
-                <form class="form" action="process.php" method="POST">
-                    
-                <input type="hidden" name="id" value="<?php echo $id; ?> ">
-                
-                <input class="form-item input" type="text" name="name" 
-                    value="<?php echo $name; ?>" placeholder="Enter your name">
-                    <input class="form-item input" type="text" name="location"
-                    value="<?php echo $location; ?>"  placeholder="Enter your location">
-                    <?php 
-                        if ($update == true):
-                    ?>
-                         <button class="form-item btn btn-save" type="submit" name="update">Update</button>
-                
-                    <?php else :?>
-                        <button class="form-item btn btn-save" type="submit" name="save">Save</button>
-                    <?php endif; ?>
-                
-                </form>
-            </div>
-
+</div>
 
 
 
